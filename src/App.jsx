46d328ea90c4d39d8f9fd0789b5bba6dcbfc6e0d
RyukShi <aure16300P@gmail.com>
2023-03-27 16:58:38 +0200

    New filter, title and original title

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import './App.css'
 import DataContext from './context/DataContext'
 import FilmCard from './components/FilmCard'
 
 function App() {
   const { loading, films } = useContext(DataContext)
+  const [search, setSearch] = useState("")
 
-  const showFilms = films.map(film =>
-    <FilmCard key={film.id} film={film} />
-  )
+  const handleChange = (e) => {
+    setSearch(e.target.value.toLowerCase())
+  }
+
+  const showFilms = films.filter(film =>
+    film.title.toLowerCase().includes(search) ||
+    film.original_title.toLowerCase().includes(search)
+  ).map(film => <FilmCard key={film.id} film={film} />)
+
   return (
     <div className="App">
       {(loading) && (
@@ -16,8 +23,12 @@ function App() {
       )}
 
       {(films.length > 0) && (
-        <div className="film-grid">
-          {showFilms}
+        <div>
+          <input type="text" onChange={handleChange}
+            placeholder="Enter title or original title" />
+          <div className="film-grid">
+            {showFilms}
+          </div>
         </div>
       )}
     </div>
