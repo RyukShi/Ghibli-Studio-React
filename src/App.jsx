095e8808e807c5defeeb4b8991448a36c34099de
RyukShi <aure16300P@gmail.com>
2023-03-28 13:57:16 +0200

    New filter, director

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,36 @@ import FilmCard from './components/FilmCard'
 function App() {
   const { loading, films } = useContext(DataContext)
   const [search, setSearch] = useState("")
+  const [director, setDirector] = useState("")
 
-  const handleChange = (e) => {
+  const handleChangeSearch = (e) => {
     setSearch(e.target.value.toLowerCase())
   }
 
-  const showFilms = films.filter(film =>
-    film.title.toLowerCase().includes(search) ||
-    film.original_title.toLowerCase().includes(search)
-  ).map(film => <FilmCard key={film.id} film={film} />)
+  const handleChangeDirector = (e) => {
+    setDirector(e.target.value)
+  }
+
+  const showFilms = films
+    .filter(film =>
+      (search ?
+        (film.title.toLowerCase().includes(search) || film.original_title.toLowerCase().includes(search))
+        : true) &&
+      (director ? film.director === director : true))
+    .map(film => <FilmCard key={film.id} film={film} />)
+
+  /* Extract unique director from films array and create options */
+  const DIRECTORS_OPTIONS = Array.from(new Set(films.map(f => f.director)))
+    .map(d => <option key={d} value={d}>{d}</option>)
+
+  const filterDivCSS = {
+    display: 'flex',
+    justifyContent: 'center',
+    columnGap: 30,
+    alignItems: 'center',
+    width: '100%',
+    margin: '40px 0 40px 0'
+  }
 
   return (
     <div className="App">
@@ -24,8 +45,16 @@ function App() {
 
       {(films.length > 0) && (
         <div>
-          <input type="text" onChange={handleChange}
-            placeholder="Enter title or original title" />
+          <div style={filterDivCSS}>
+            <input type="text" onChange={handleChangeSearch}
+              placeholder="Enter title or original title" />
+            <select
+              value={director}
+              onChange={handleChangeDirector}>
+              <option value="">Choose director to filter</option>
+              {DIRECTORS_OPTIONS}
+            </select>
+          </div>
           <div className="film-grid">
             {showFilms}
           </div>
